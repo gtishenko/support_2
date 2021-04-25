8c7e84efba68738109d44332fb9a1f50a577f3c1
gtishenko <gregorytyshchenko3011@gmail.com>
2021-04-25 17:51:33 +0300

    Updates

---
+++ precache-manifest.bf112e8dd52c7deb973cd4a210e96d1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d2ba5c7081b86e5dd147",
+    "url": "/support_2/static/css/main.8d5c3930.chunk.css"
+  },
+  {
+    "revision": "d2ba5c7081b86e5dd147",
+    "url": "/support_2/static/js/main.426dd61f.chunk.js"
+  },
+  {
+    "revision": "d7fcf7c12e828c9c5f83",
+    "url": "/support_2/static/js/runtime~main.82285d1f.js"
+  },
+  {
+    "revision": "6800641b68e9921acd12",
+    "url": "/support_2/static/css/2.58ebec9f.chunk.css"
+  },
+  {
+    "revision": "6800641b68e9921acd12",
+    "url": "/support_2/static/js/2.98bd21e6.chunk.js"
+  },
+  {
+    "revision": "9031c03b4053f1fa419665c8652911fc",
+    "url": "/support_2/index.html"
+  }
+];
